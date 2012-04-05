def4e3396fa35cf12f1cd14fca6a2558a5dfd384
Bryan Mahoney <bryan@goDynamo.com>
2012-04-05 10:59:32 -0400

    Scopes images to the current user that created them

--- app/decorators/controllers/refinery/images_controller_decorator.rb
+++ app/decorators/controllers/refinery/images_controller_decorator.rb
@@ -1,15 +1,20 @@
 ::Refinery::Admin::ImagesController.class_eval do
 
-  def ajax_create
+def ajax_create
     params[:image] = {:image => params[:image]}
     params[:image].each do  |image|
-      @image = ::Refinery::Image.create(params[:image])
+      @image = current_refinery_user.images.create(params[:image])
     end
     unless @image.errors.any?
       render :json => { :redirect_to => refinery.admin_images_path }
     else
       render :json => @image.errors, :status => 406
     end
+  end
+
+  def find_all_images(conditions = '')
+    @images = ::Refinery::Image.where(conditions).order("created_at DESC") if current_refinery_user.has_role?(:superuser)
+    @images = current_refinery_user.images.order("created_at DESC") if !current_refinery_user.has_role?(:superuser)
   end
 
 end
